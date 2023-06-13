6479c1883501054b88a756a991f4efa38bff462c
leader03 <subedibibek2000@gmail.com>
2023-06-13 07:27:08 +0545

    header footer

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Button from "./Button";
+import Start from "./Home";
+
+export {
+    Header,
+    Footer,
+    Button,
+    Start,
+}
