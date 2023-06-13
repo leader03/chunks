29722b2ebd5dbbd90c37bec6fa85a365ba158ab1
leader03 <subedibibek2000@gmail.com>
2023-06-13 07:40:53 +0545

    search filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
 import Image from 'next/image'
 import Start from '@/components/Home'
 
+import { Search, Filter } from '@/components'
+import { fuels, yearsOfProduction } from '@/constants'
+
 export default function Home() {
   return (
     <main className=' overflow-hidden text-red-600'>
       <Start />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <Search />
+
+          <div className='home__filter-container'>
+            <Filter title='fuel' options={fuels} />
+            <Filter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,14 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Button from "./Button";
 import Start from "./Home";
+import Search from "./Search";
+import Filter from "./Filter";
 
 export {
     Header,
     Footer,
     Button,
     Start,
+    Search,
+    Filter,
 }
